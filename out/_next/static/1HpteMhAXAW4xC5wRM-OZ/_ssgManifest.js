21fc25f289b065c6d855ad3c3ada62949aaf5f1e
Nekodigi <38092328+Nekodigi@users.noreply.github.com>
2022-11-15 20:55:24 +0900

    removed unnecessary file

--- out/_next/static/1HpteMhAXAW4xC5wRM-OZ/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Farchive\u002Fall\u002F[page]","\u002Farchive\u002Fmonthly\u002F[YYMM]\u002F[page]","\u002Fpost\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
