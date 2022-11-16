02c6831d036336d2a41f93f8aa9f28fcda1c1158
Nekodigi <38092328+Nekodigi@users.noreply.github.com>
2022-11-16 11:18:26 +0900

    github action check

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,14 +3,14 @@ import Posts from "components/posts/posts";
 import SidePage from "components/sidePage";
 
 export default function index({ posts, counts }) {
-    return (
-      <div>
-        <Posts posts={posts.posts} statusMessage={`お探しのページが見つかりませんでした。`}/>
-        <SidePage prev={posts.next_id ? posts.next_id.toString() : undefined} next={posts.prev_id ? posts.prev_id.toString() : undefined}/>
-        <MonthlyArchive counts={counts}/>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Posts posts={posts.posts} statusMessage={`お探しのページが見つかりませんでした。`}/>
+      <SidePage prev={posts.next_id ? posts.next_id.toString() : undefined} next={posts.prev_id ? posts.prev_id.toString() : undefined}/>
+      <MonthlyArchive counts={counts}/>
+    </div>
+  );
+}
   
 export async function getStaticProps({ params }) {
   const posts = await(await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/archive?page=1&n=${2}`)).json();
